d17ae840ed6262b1b235b9821cf3c7c73889d95c
Matt Robenolt <matt@ydekproductions.com>
2014-08-04 03:25:29 +0000

    Fundamentally wire up api to what graphite-cyanite expects

--- api/api.go
+++ api/api.go
@@ -3,6 +3,7 @@ package api
 import (
 	"github.com/mattrobenolt/go-cyanite/store"
 
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -10,6 +11,25 @@ import (
 func Ping(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte("{}"))
+}
+
+func Paths(w http.ResponseWriter, r *http.Request) {
+	log.Println(r)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `[
+		{"leaf": true, "path": "%s"}
+	]`, r.URL.Query().Get("query"))
+}
+
+func Metrics(w http.ResponseWriter, r *http.Request) {
+	log.Println(r)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{
+		"from": %s,
+		"to": %s,
+		"step": 10,
+		"series": {"%s": []}
+	}`, r.URL.Query().Get("from"), r.URL.Query().Get("to"), r.URL.Query().Get("path"))
 }
 
 func ListenAndServe(addr string) error {
@@ -20,5 +40,7 @@ func ListenAndServe(addr string) error {
 
 	http.HandleFunc("/ping/", Ping)
 	http.HandleFunc("/ping", Ping)
+	http.HandleFunc("/metrics", Metrics)
+	http.HandleFunc("/paths", Paths)
 	panic(http.ListenAndServe(addr, nil))
 }
