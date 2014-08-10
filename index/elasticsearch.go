bd951bdff705b6535d5ee4f8286b8514a7fc7cd3
Matt Robenolt <matt@ydekproductions.com>
2014-08-10 22:20:52 +0000

    Bootstrap Elasticsearch schema on Init and finalize schema to use

--- index/elasticsearch.go
+++ index/elasticsearch.go
@@ -6,6 +6,7 @@ import (
 
 	"log"
 	"net/url"
+	"strings"
 )
 
 type ElasticSearchDriver struct {
@@ -19,6 +20,15 @@ func (d *ElasticSearchDriver) Init(url *url.URL) (err error) {
 	conn := elastigo.NewConn()
 	conn.Domain = url.Host
 	d.index = url.Path[1:]
+	if ok, _ := conn.ExistsIndex(d.index, "", nil); !ok {
+		log.Println("Creating new index in Elasticsearch...")
+		resp, err := conn.DoCommand("PUT", "/"+d.index, nil, schema)
+		log.Println(string(resp))
+		if err != nil {
+			panic(err)
+		}
+		log.Println("ok.")
+	}
 	d.cache = set.NewThreadUnsafeSet()
 	d.conn = conn.NewBulkIndexer(10)
 	d.conn.Start()
@@ -31,8 +41,21 @@ func (d *ElasticSearchDriver) Update(path string) error {
 		return nil
 	}
 	d.cache.Add(path)
-	d.conn.Index(d.index, "path", path, "", nil, Path{path}, false)
-	log.Println("Updating index", path)
+	end := len(path)
+	depth := strings.Count(path, ".")
+	leaf := true
+	var p Path
+	for end > -1 {
+		path = path[0:end]
+		p.Key = path
+		p.Depth = depth
+		p.Leaf = leaf
+		// ignoring errors for now
+		d.conn.Index(d.index, "path", p.Key, "", nil, p, false)
+		end = strings.LastIndex(path, ".")
+		depth--
+		leaf = false
+	}
 	return nil
 }
 
@@ -47,5 +70,37 @@ func init() {
 }
 
 type Path struct {
-	Path string
+	Key   string
+	Depth int
+	Leaf  bool
+}
+
+var schema = map[string]interface{}{
+	"settings": map[string]interface{}{
+		"analysis": map[string]interface{}{
+			"analyzer": map[string]interface{}{
+				"mineshaft-analyzer": map[string]string{
+					"type":      "custom",
+					"tokenizer": "mineshaft-tokenizer",
+				},
+			},
+			"tokenizer": map[string]interface{}{
+				"mineshaft-tokenizer": map[string]string{
+					"type":      "path_hierarchy",
+					"delimiter": ".",
+				},
+			},
+		},
+	},
+	"mappings": map[string]interface{}{
+		"doc": map[string]interface{}{
+			"properties": map[string]interface{}{
+				"Key": map[string]string{
+					"type":            "string",
+					"index_analyzer":  "mineshaft-analyzer",
+					"search_analyzer": "keyword",
+				},
+			},
+		},
+	},
 }
